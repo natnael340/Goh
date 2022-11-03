4ba8b178973520691176e7ad86ac38cd8790d9b3
natnael tilahun <40854555+natnael340@users.noreply.github.com>
2022-11-03 13:46:35 +0000

    inserted default admins creads and added bcrypt package

--- db.js
+++ db.js
@@ -1,4 +1,6 @@
 const sqlite = require('sqlite3');
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
 
 let db = new sqlite.Database('./db.sqlite', sqlite.OPEN_READWRITE, (e) => {
   if (e) {
@@ -15,6 +17,16 @@ db.serialize(() => {
   db.run("CREATE TABLE IF NOT EXISTS review(id INTEGER PRIMARY KEY AUTOINCREMENT, uid INTEGER NOT NULL, pid INTEGER NOT NULL, comment TEXT NOT NULL, created_date TEXT NOT NULL, FOREIGN KEY(pid) REFERENCES products(id) ON UPDATE CASCADE ON DELETE CASCADE, FOREIGN KEY(uid) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE)");
   db.run("CREATE TABLE IF NOT EXISTS user_fav(id INTEGER PRIMARY KEY AUTOINCREMENT, uid INTEGER NOT NULL, pid INTEGER NOT NULL, FOREIGN KEY(pid) REFERENCES products(id) ON UPDATE CASCADE ON DELETE CASCADE, FOREIGN KEY(uid) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE)");
   db.run("CREATE TABLE IF NOT EXISTS user_fav(id INTEGER PRIMARY KEY AUTOINCREMENT, uid INTEGER NOT NULL, pid INTEGER NOT NULL, FOREIGN KEY(pid) REFERENCES products(id) ON UPDATE CASCADE ON DELETE CASCADE, FOREIGN KEY(uid) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE)");
-}
-);
+  //db.run(`INSERT INTO users(username, full_name, created_date, contact, active) VALUES('malik', 'Natnael Tilahun','${(new Date()).getDate()}/${(new Date()).getMonth()+1}/${(new Date()).getFullYear()}-${(new Date()).getHours()}:${(new Date()).getMinutes()}:${(new Date()).getSeconds()}','${JSON.stringify({phone_number: "0926600549", city: "Addis Abeba", street: "bole"})}',1),('nolawit', 'Fiker Yibeltal','${(new Date()).getDate()}/${(new Date()).getMonth()+1}/${(new Date()).getFullYear()}-${(new Date()).getHours()}:${(new Date()).getMinutes()}:${(new Date()).getSeconds()}','${JSON.stringify({phone_number: "0911532515", city: "Addis Abeba", street: "22"})}',1)`);
+ // db.run(`INSERT INTO login(uid, email, password, mfa) VALUES(1, 'natnaeltilahun157@gmail.com', '${bcrypt.hashSync("malik", salt)}',0),(2, 'fikeryibeltal31@gmail.com', '${bcrypt.hashSync("nolawit", salt)}',0)`);
+  db.all("SELECT * FROM login", [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row);
+  });
+});
+
+});
 db.close();
