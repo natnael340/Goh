5095bac4f6877bb5135ee26e118e6f2dbd9ddd9e
natnael tilahun <40854555+natnael340@users.noreply.github.com>
2022-11-03 12:23:37 +0000

    Removing node_modules

--- db.js
+++ db.js
@@ -8,7 +8,9 @@ let db = new sqlite.Database('./db.sqlite', sqlite.OPEN_READWRITE, (e) => {
 });
 
 db.serialize(() => {
-  db.run("CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT, full_name TEXT, created_date TEXT, contact TEXT, active INTEGER");
-  db.run("CREATE TABLE blocked_user(id INTEGER PRIMARY KEY AUTOINCREMENT, full_name TEXT, created_date TEXT, contact TEXT, active INTEGER");
+  db.run("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,username TEXT NOT NULL UNIQUE, full_name TEXT, created_date TEXT, contact TEXT, active INTEGER NOT NULL)");
+  db.run("CREATE TABLE IF NOT EXISTS blocked_user(id INTEGER PRIMARY KEY AUTOINCREMENT, uid INTEGER, created_date TEXT NOT NULL, blocked_for TEXT NOT NULL, ip TEXT NOT NULL, desc TEXT, FOREIGN KEY(uid) REFERENCES users(id))");
+  db.run("CREATE TABLE IF NOT EXISTS login(id INTEGER PRIMARY KEY AUTOINCREMENT, uid INTEGER NOT NULL, created_date TEXT NOT NULL, blocked_for TEXT NOT NULL, ip TEXT NOT NULL, desc TEXT, FOREIGN KEY(uid) REFERENCES users(id))")
 }
-)
+);
+db.close();
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express()
 
 const port = 8080
-require('./db.js')
+//require('./db.js')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
